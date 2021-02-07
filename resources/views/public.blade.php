b1cc8a928d6992720952c6b2f296e4cf439a9a70
Kiran Anand <liteninkiran@hotmail.com>
2021-02-07 14:45:29 +0000

    Adding overview

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -30,6 +30,7 @@
                         <ul>
                             <li><a href="#home" data-after="Home">Home</a></li>
                             <li><a href="#overview" data-after="Overview">Overview</a></li>
+                            <li><a href="#projects" data-after="Projects">Projects</a></li>
 <!--                             <li><a href="#calibration" data-after="Calibration">Calibration</a></li>
                             <li><a href="#services" data-after="Service">Services</a></li>
                             <li><a href="#vacancies" data-after="Vacancies">Vacancies</a></li>
@@ -67,7 +68,7 @@
                     <p>The laboratories within <span>SCL</span> are all in a close controlled air-conditioned environment with ESD protected work stations. <span>SCL</span> have structured the laboratories into dedicated sections, which maximises the control and planning of instrumentation throughput. We currently operate 4 laboratories which are divided as follows:</p>
                 </div>
 
-                <div class="overview-bottom">
+                <div class="overview-middle">
 
                     <div class="overview-item">
                         <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png"/></div>
@@ -112,6 +113,12 @@
 
                 </div>
 
+                <div class="overview-bottom">
+                    <p><strong>Repairs</strong> are often overlooked when companies are budgeting to have their test equipment calibrated. For many reasons, most equipment will require repair at some stage, normally at a most inconvenient time. To reduce down-time and subsequent loss in production, <span>SCL</span> have a team of highly skilled engineers who are experienced in fault finding to component level on any test equipment, from AVO meters to complex Spectrum Analysers.</p>
+                    <p><strong>Confidence</strong> in our ability in this field is supported by AVO / Megger Group Limited. They have registered <span>SCL</span> as an Approved Service Centre, having assessed our organisation as complying with the requirements of Quality Assurance Standard OAS/005 REG No.016.</p>
+                    <p class="final">SCL BELIEVE THAT THE COST OF CALIBRATION CAN BE STRONGLY OVERSHADOWED BY AN INEFFICIENT REPAIR SERVICE.</p>
+                </div>
+
             </div>
 
         </section>
@@ -119,6 +126,68 @@
 
 
 
+
+<!-- Projects Section -->
+<section id="projects">
+<div class="projects container">
+<div class="projects-header">
+<h1 class="section-title">Recent <span>Projects</span></h1>
+</div>
+<div class="all-projects">
+<div class="project-item">
+<div class="project-info">
+<h1>Project 1</h1>
+<h2>Coding is Love</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+</div>
+<div class="project-img">
+<img src="./img/img-1.png" alt="img">
+</div>
+</div>
+<div class="project-item">
+<div class="project-info">
+<h1>Project 2</h1>
+<h2>Coding is Love</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+</div>
+<div class="project-img">
+<img src="./img/img-1.png" alt="img">
+</div>
+</div>
+<div class="project-item">
+<div class="project-info">
+<h1>Project 3</h1>
+<h2>Coding is Love</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+</div>
+<div class="project-img">
+<img src="./img/img-1.png" alt="img">
+</div>
+</div>
+<div class="project-item">
+<div class="project-info">
+<h1>Project 4</h1>
+<h2>Coding is Love</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+</div>
+<div class="project-img">
+<img src="./img/img-1.png" alt="img">
+</div>
+</div>
+<div class="project-item">
+<div class="project-info">
+<h1>Project 5</h1>
+<h2>Coding is Love</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+</div>
+<div class="project-img">
+<img src="./img/img-1.png" alt="img">
+</div>
+</div>
+</div>
+</div>
+</section>
+<!-- End Projects Section -->
 
 
 
