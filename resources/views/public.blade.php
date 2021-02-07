ca244b83e120b129114340b63834f0ff2bde5136
Kiran Anand <liteninkiran@hotmail.com>
2021-02-07 17:38:10 +0000

    Adding other sections

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -30,12 +30,11 @@
                         <ul>
                             <li><a href="#home" data-after="Home">Home</a></li>
                             <li><a href="#overview" data-after="Overview">Overview</a></li>
-                            <li><a href="#projects" data-after="Projects">Projects</a></li>
-<!--                             <li><a href="#calibration" data-after="Calibration">Calibration</a></li>
+                            <li><a href="#calibration" data-after="Calibration">Calibration</a></li>
                             <li><a href="#services" data-after="Service">Services</a></li>
                             <li><a href="#vacancies" data-after="Vacancies">Vacancies</a></li>
                             <li><a href="#contact" data-after="Contact">Contact</a></li>
- -->                        </ul>
+                        </ul>
 
                     </div>
 
@@ -125,72 +124,183 @@
         <!-- End Overview Section -->
 
 
+        <!-- Calibration Section -->
+        <section id="calibration">
 
+            <div class="calibration container">
 
-<!-- Projects Section -->
-<section id="projects">
-<div class="projects container">
-<div class="projects-header">
-<h1 class="section-title">Recent <span>Projects</span></h1>
-</div>
-<div class="all-projects">
-<div class="project-item">
-<div class="project-info">
-<h1>Project 1</h1>
-<h2>Coding is Love</h2>
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
-</div>
-<div class="project-img">
-<img src="./img/img-1.png" alt="img">
-</div>
-</div>
-<div class="project-item">
-<div class="project-info">
-<h1>Project 2</h1>
-<h2>Coding is Love</h2>
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
-</div>
-<div class="project-img">
-<img src="./img/img-1.png" alt="img">
-</div>
-</div>
-<div class="project-item">
-<div class="project-info">
-<h1>Project 3</h1>
-<h2>Coding is Love</h2>
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
-</div>
-<div class="project-img">
-<img src="./img/img-1.png" alt="img">
-</div>
-</div>
-<div class="project-item">
-<div class="project-info">
-<h1>Project 4</h1>
-<h2>Coding is Love</h2>
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
-</div>
-<div class="project-img">
-<img src="./img/img-1.png" alt="img">
-</div>
-</div>
-<div class="project-item">
-<div class="project-info">
-<h1>Project 5</h1>
-<h2>Coding is Love</h2>
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
-</div>
-<div class="project-img">
-<img src="./img/img-1.png" alt="img">
-</div>
-</div>
-</div>
-</div>
-</section>
-<!-- End Projects Section -->
+                <div class="calibration-top">
+                    <h1 class="section-title"><span>C</span>alibration</h1>
+                    <p>This website does not contain glossy pictures of Nuclear Submarines or Space Stations, but simply factual information. Most companies appreciate the possible repercussion costs of not having instrumentation calibrated. It is vital therefore that the cost of calibration - an expense with no perceived return - is not simply paying for a Certificate of Calibration. We at <span>Southern Calibration Laboratories</span> believe that quality is <i>paramount</i> and that the time taken to read the following could well save you a lot of time and money in the future:</p>
+                    <ul>
+                        <li>UKAS Accredited</li>
+                        <li>Test results, instruments used and uncertainties provided as standard</li>
+                        <li>We test the <strong>full range</strong> of a unit's measuring capabilities, recording exact results to the unit's <strong>maximum resolution</strong></li>
+                        <li>Calibration measurements are routinely audited in accordance with internal policies</li>
+                    </ul>
+                    <p>By using <span>SCL</span>, you will receive the highest level of detail for the performance of your equipment, giving you the confidence to make measurements accurately.</p>
+                </div>
+
+            </div>
+
+        </section>
+        <!-- End Calibration Section -->
 
 
 
+        <!-- Services Section -->
+        <section id="services">
+
+            <div class="services container">
+
+                <div class="services-top">
+                    <h1 class="section-title"><span>S</span>ervices</h1>
+                    <p>Our high operating standards are supported by UKAS Accreditation in both Electrical / Electronic and Dimensional / Mechanical providing a level of competence and technical expertise of the highest degree. We are dedicated in providing a total quality service. This is supported by registration of BS EN ISO9001 & BS EN ISO/IEC 17025. Our services include:</p>
+                    <ul>
+                        <li>UKAS certification</li>
+                        <li>Certification traceable to national standards</li>
+                        <li>5 working day turnaround (less in exceptional circumstances)</li>
+                        <li>Collection and delivery within a large radius</li>
+                        <li>Recall system, "calibration due" reminder letters</li>
+                        <li>All instruments are fully insured whilst in our possession</li>
+                        <li>All instruments are safety tested where applicable</li>
+                        <li>Copy of test results instruments used & uncertainties</li>
+                        <li>Adjustments where necessary (some exceptions)</li>
+                        <li>Before and after results</li>
+                    </ul>
+                    <p>Should an instrument fail because it is faulty, the customer would be contacted before the item is due to be returned as to the nature of the fault, together with an estimate for repairs. <strong><i>No repair charges are made without the prior consent of the customer.</i></strong></p>
+                </div>
+
+                <hr>
+
+                <div class="services-top">
+                    <p>To further enhance our calibration and repair service we are also able to source/supply new test equipment from almost all manufacturers. The benefits to our customers are:</p>
+                    <ul>
+                        <li>Advise on product selection, as we are totally independent</li>
+                        <li>Single Source supply/calibration often at discounted rates</li>
+                        <li>Less administration leading to cost effective budgeting</li>
+                    </ul>
+                </div>
+
+
+            </div>
+
+        </section>
+        <!-- End Services Section -->
+
+
+
+        <!-- Vacancies Section -->
+        <section id="vacancies">
+
+            <div class="vacancies container">
+
+                <div class="vacancies-top">
+                    <h1 class="section-title"><span>V</span>acancies</h1>
+                    <p>We currently have the following immediate and permanent vacancies:</p>
+                    <ul>
+                        <li>Calibration Engineers</li>
+                        <p>Duties include calibrating a wide range of test and measuring equipment. The job can involve occasional working onsite at customer’s premises. Consideration will be given to candidates that have a background of working in an electrical calibration or test environment (preferably with RF experience) or have a suitable electronics, physics or engineering qualification. Fault finding to component level will be a distinct advantage. Training will be provided to the suitable applicant.</p>
+                        <li>Stores Administrator/Relief Driver</li>
+                        <p>Duties include general goods in/goods out functions include booking customer equipment in, arranging and liaising with sub-contractors, raising Purchase Orders, ordering spares etc. You will also be required to provide relief cover for the company delivery drivers during periods of leave or absence. Training will be provided. The suitable candidate will have a previous background of working in a similar environment, with sound computer, numeracy, and literary skills, with a clean driver’s license.</p>
+                    </ul>
+
+                    <p>If you think you have the necessary skills in either of these roles, please forward your CV to <a href="applications@southcal.co.uk">applications@southcal.co.uk</a>.</p>
+                    <p>Please Note: if you have not received a response from us within two weeks, please assume that on this occasion, you have not been shortlisted. Thank you for your interest.</p>
+                </div>
+
+            </div>
+
+        </section>
+        <!-- End Vacancies Section -->
+
+
+
+        <!-- Contact Section -->
+        <section id="contact">
+
+            <div class="contact container">
+
+                <div><h1 class="section-title">Contact <span>info</span></h1></div>
+
+                <div class="contact-items">
+
+                    <div class="contact-item">
+                        <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/phone.png"/></div>
+                        <div class="contact-info">
+                            <h1>Phone / Fax</h1>
+                            <h2>P: 01489 790296</h2>
+                            <h2>F: 01489 790294</h2>
+                        </div>
+                    </div>
+
+                    <div class="contact-item">
+                        <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png"/></div>
+                        <div class="contact-info">
+                            <h1>Email</h1>
+                            <h2><a href="enquiries@southcal.co.uk">enquiries@southcal.co.uk</a></h2>
+                        </div>
+                    </div>
+
+                    <div class="contact-item">
+                        <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png"/></div>
+                        <div class="contact-info">
+                            <h1>Address</h1>
+                            <h2>Solent Industrial Estate Unit 7,</h2>
+                            <h2>Shamblehurst Lane,</h2>
+                            <h2>Hedge End,</h2>
+                            <h2>Southampton,</h2>
+                            <h2>SO30 2FX</h2>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div style="margin-top: 50px">
+                    <img src="{{ URL::to('/') }}/assets/map.gif"/>
+                </div>
+
+            </div>
+
+        </section>
+        <!-- End Contact Section -->
+
+
+
+        <!-- Footer -->
+        <section id="footer">
+
+            <div class="footer container">
+
+                <div class="brand">
+                    <h1><span>K</span>iran <span>A</span>nand</h1>
+                </div>
+
+                <div class="social-icon">
+
+                    <div class="social-item">
+                        <a href="https://github.com/liteninkiran/"><img src="https://img.icons8.com/bubbles/100/000000/github.png"/></a>
+                    </div>
+
+                    <div class="social-item">
+                        <a href="https://www.linkedin.com/in/kirananand/"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png"/></a>
+                    </div>
+
+                    <div class="social-item">
+                        <a href="https://www.facebook.com/KiranAnand"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png"/></a>
+                    </div>
+
+                    <div class="social-item">
+                        <a href="https://twitter.com/Kiran_Anand"><img src="https://img.icons8.com/bubbles/100/000000/twitter.png"/></a>
+                    </div>
+
+                </div>
+
+                <p>Copyright © 2020 Kiran Anand</p>
+        
+            </div>
+
+        </section>
 
         <!-- Scripts -->
         <script type="text/javascript" src="{{ asset('js/scl-app.js') }}"></script>
